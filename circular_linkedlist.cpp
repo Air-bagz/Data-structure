506f141e822eba4ffc6b46ae8fb30cde0abc5a66
Air-bagz <97088312+Air-bagz@users.noreply.github.com>
2022-01-06 14:37:28 +0800

    Add files via upload

---
+++ circular_linkedlist.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int ElemType;
+
+typedef struct LNode
+{
+	ElemType data;
+	struct LNode *next;
+}LNode,*LinkList;
+
+bool InitList(LinkList &L)
+{
+	L=(LNode *)malloc(sizeof(LNode));
+	if (L==NULL)	return false;
+	L->next=L;
+	return true;
+}
+
+bool Empty(LinkList L)				//判断循环单链表是否为空 
+{
+	if (L->next==L)	return true;
+	else return false;
+}
+
+bool isTail(LinkList L,LNode *p)	//判断结点p是否为循环单链表的表尾结点 
+{
+	if (p->next==L)	return true;
+	else return false;
+}
+
+int main()
+{
+	LinkList L;
+	InitList(L);
+ } 
