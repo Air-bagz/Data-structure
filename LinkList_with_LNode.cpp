8829e3aad44b38369615bf5ed4d749b884331f2b
Air-bagz <97088312+Air-bagz@users.noreply.github.com>
2022-01-07 00:59:06 +0800

    Update LinkList_with_LNode.cpp

--- LinkList_with_LNode.cpp
+++ LinkList_with_LNode.cpp
@@ -1,4 +1,4 @@
-//��ͷ���ĵ����� 
+//带头结点的单链表 
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -11,13 +11,16 @@ typedef struct LNode
 }LNode,*LinkList;
 
 bool InitList(LinkList &L);
+void DestroyList(LinkList &L);
 LNode *GetElem(LinkList L,int i);				//i in [0,\infty]	O(n)
 bool InsertNextNode(LNode *p,ElemType e);		 //p is key			O(1)
 bool ListInsert(LinkList &L,int i,ElemType e);	//i in [1,\infty]	O(n)
 bool ListDelete(LinkList &L,int i,ElemType &e); //i in [1,\infty]	O(n)
+bool DeleteNode1(LinkList &L,LNode *p);
+bool DeleteNode2(LNode *p);
 LNode *LocateElem(LinkList L,ElemType e);
-void PrintList(LinkList L); 
 int Length(LinkList L);
+void PrintList(LinkList L); 
 
 int main()
 {
@@ -27,28 +30,48 @@ int main()
 	PrintList(L);
 	
 	int e=-1;
-	if (ListDelete(L,2,e))	printf("��ɾ����2��Ԫ�أ�ֵΪ%d\n",e);
-		else printf("ɾ��ʧ��");
+	if (ListDelete(L,2,e))	printf("已删除第2个元素，值为%d\n",e);
+		else printf("删除失败");
 	PrintList(L);
 	
-	LNode *p=LocateElem(L,303);		printf("%d\n",p->data);
+	LNode *p=LocateElem(L,303);		printf("  %d\n",p->data);
 	
 	printf("length=%d\n",Length(L));
+	
+	DeleteNode1(L,p);	printf("删除p结点后length=%d\n",Length(L));		PrintList(L);	
+	
+	LNode *p2=LocateElem(L,101);	DeleteNode2(p2);	printf("删除p结点后length=%d\n",Length(L));		PrintList(L);	//剩一个结点删除失灵 
+	
+	DestroyList(L);		
+	
 	return 0; 
 }
 
 bool InitList(LinkList &L)
 {
-	L=(LNode *)malloc(sizeof(LNode));
+	L=(LNode *)malloc(sizeof(LNode));	
 	if (L==NULL)	return false;
 	L->next=NULL;
 	return true;
 }
 
+void DestroyList(LinkList &L)
+{	
+	LNode *q=L->next;
+	while (q!=NULL)		//只要后继不空，就删后续q 
+	{
+		L->next=q->next;
+		free(q);			
+		q=L->next;
+	}
+	printf("销毁链表后length=%d\n",Length(L));
+	free(L);		//删除头结点 
+}
+
 LNode *GetElem(LinkList L,int i)
 {
 	if(i<0)	return NULL;
-	LNode *p=L;
+	LNode *p=L;		//从第0个结点开始 
 	int j=0;
 	while (p!=NULL && j<i)	//until j==i or p==NULL
 	{
@@ -89,25 +112,38 @@ bool ListDelete(LinkList &L,int i,ElemType &e)
 	return true;
 }
 
+bool DeleteNode1(LinkList &L,LNode *p)
+{
+	LNode *pp=L;
+	while (pp->next != p)
+		pp=pp->next;
+	pp->next=p->next;
+	free(p);
+ } 
+
+bool DeleteNode2(LNode *p)
+{
+	if (p==NULL)	return false;	//空的删失败 
+	if (p->next==NULL)	return false;	//最后一个删失败 
+	LNode *q=p->next;
+	p->data=q->data;
+	p->next=q->next;
+	free(q);
+	return true; 
+}
+
 LNode *LocateElem(LinkList L,ElemType e)
 {
-	LNode *p=L->next;
+	LNode *p=L->next;	//从第1个结点开始找,p!=NULL
 	while (p!=NULL && p->data!=e)
 		p=p->next;
 	return p;
 }
 
-void PrintList(LinkList L)
-{
-	LNode *p;
-	for (p=L->next;p!=NULL;p=p->next)
-		printf("%d\n",p->data);
-}
-
 int Length(LinkList L)
 {
 	int len=0;
-	LNode *p=L;
+	LNode *p=L;		//从第0个结点开始,p->next!=NULL
 	while (p->next !=NULL)
 	{
 		p=p->next;
@@ -115,3 +151,11 @@ int Length(LinkList L)
 	}
 	return len;
 }
+
+void PrintList(LinkList L)
+{
+	LNode *p;
+	for (p=L->next;p!=NULL;p=p->next)	//从第1个结点开始,p!=NULL
+		printf("%d\n",p->data);
+}
+//其实统一化都应该从第0个结点(表头)开始，因为单链表可能只有表头，无伤大雅 
