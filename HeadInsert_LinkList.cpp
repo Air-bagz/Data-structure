f32570f49ff858fe605c2962bb1f96db18afb74b
Air-bagz <97088312+Air-bagz@users.noreply.github.com>
2022-01-07 11:39:55 +0800

    Update HeadInsert_LinkList.cpp

--- HeadInsert_LinkList.cpp
+++ HeadInsert_LinkList.cpp
@@ -18,6 +18,47 @@ while ()
 }
 */
 
+void InitList(LinkList &L);
+void InsertNextNode(LNode *p,ElemType e);
+void List_HeadInsert2(LinkList &L);
+LinkList List_HeadInsert(LinkList &L);
+void PrintList(LinkList L);
+
+int main()
+{
+	LinkList L;
+	//L=List_HeadInsert(L);
+	InitList(L);
+	List_HeadInsert2(L);
+	PrintList(L);
+	return 0;
+}
+
+void InitList(LinkList &L)
+{
+	L=(LNode *)malloc(sizeof(LNode));	
+	L->next=NULL;
+}
+
+void InsertNextNode(LNode *p,ElemType e)
+{
+	LNode *s=(LNode *)malloc(sizeof(LNode));
+	s->data=e;
+	s->next=p->next;
+	p->next=s;
+}
+
+void List_HeadInsert2(LinkList &L)
+{
+	int x;
+	scanf("%d",&x);
+	while (x!=9999)
+	{
+		InsertNextNode(L,x);
+		scanf("%d",&x);
+	}
+}
+
 LinkList List_HeadInsert(LinkList &L)
 {
 	LNode *s;
@@ -41,12 +82,4 @@ void PrintList(LinkList L)
 	LNode *p;
 	for (p=L->next;p!=NULL;p=p->next)
 		printf("%d\n",p->data);
-}
-
-int main()
-{
-	LinkList L;
-	L=List_HeadInsert(L);
-	PrintList(L);
-	return 0;
 }
