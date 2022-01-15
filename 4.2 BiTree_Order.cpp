9abbf8d09992e4e9eaaa66b4ae2578ec536cee81
Air-bagz <97088312+Air-bagz@users.noreply.github.com>
2022-01-15 19:19:18 +0800

    Update and rename 4.2 BiTree_with_BiTNode.cpp to 4.2 BiTree_Order.cpp

--- 4.2 BiTree_Order.cpp
+++ 4.2 BiTree_Order.cpp
@@ -1,9 +1,7 @@
+#include<stdio.h>
 #include<stdlib.h> 
 //二叉树的结点（链式存储）
-struct ElemType
-{
-	int value;
-};
+typedef char ElemType;
 
 typedef struct BiTNode
 {
@@ -11,21 +9,40 @@ typedef struct BiTNode
 	struct BiTNode *lchild,*rchild;		//左、右孩子指针 
  }BiTNode,*BiTree;
 
+void PreOrder(BiTree T);
+void InOrder(BiTree T);
+void PostOrder(BiTree T);
+int treeDepth(BiTree T);
+void visit(BiTNode * p);
+
 int main()
 {
 	//定义一棵空树 
 	BiTree root=NULL;
 	//插入根结点
 	root=(BiTNode *)malloc(sizeof(BiTNode));
-	root->data={1};
+	root->data='A';
 	root->lchild=NULL;
 	root->rchild=NULL;
 	//插入新结点
 	BiTNode *p=(BiTNode *)malloc(sizeof(BiTNode));
-	p->data={2};
+	p->data='B';
 	p->lchild=NULL;
 	p->rchild=NULL;
 	root->lchild=p;		//作为根结点的左孩子 
+	
+	//插入新结点
+	BiTNode *p2=(BiTNode *)malloc(sizeof(BiTNode));
+	p2->data='C';
+	p2->lchild=NULL;
+	p2->rchild=NULL;
+	root->rchild=p2;		//作为根结点的右孩子 
+	
+	printf("PreOrder:\n");	PreOrder(root);		printf("\n");
+	printf("InOrder:\n");	InOrder(root);		printf("\n");
+	printf("PostOrder:\n");	PostOrder(root);	printf("\n");
+	
+	printf("depth:%d\n",treeDepth(root));
 }
 
 void PreOrder(BiTree T)				//先序遍历 
@@ -69,6 +86,12 @@ int treeDepth(BiTree T)		//后序遍历的变种
 		return l>r? l+1 : r+1; 		//树的深度=Max(左子树深度，右子树深度)+1
 	}
  } 
+ 
+void visit(BiTNode * p)
+{
+	printf("%c\n",p->data);
+}
+
 //---------三叉链表-------------
 //根据实际需求决定要不要加父结点指针 
 /*
