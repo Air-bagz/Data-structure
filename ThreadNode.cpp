b7c3e3a1811aa37d738ba4f0bd58937805fc036d
Air-bagz <97088312+Air-bagz@users.noreply.github.com>
2022-01-16 15:19:41 +0800

    Add files via upload

---
+++ ThreadNode.cpp
@@ -0,0 +1,18 @@
+//二叉树的结点（链式存储）
+typedef char ElemType;
+
+typedef struct BiTNode
+{
+	ElemType data;						//数据域 
+	struct BiTNode *lchild,*rchild;		//左、右孩子指针 
+ }BiTNode,*BiTree;
+//--------------------------------------------------------------------- 
+//线索二叉树结点
+typedef char ElemType;
+
+typedef struct ThreadNode
+{
+	ElemType data;							//数据域 
+	struct ThreadNode *lchild,*rchild;		//左、右孩子指针 
+	int ltag,rtag;							//左右线索标志 
+ }ThreadNode,*ThreadTree; 
