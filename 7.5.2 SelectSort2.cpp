3861f3d44e9b92e5c83f3175721a42e54438c650
Air-bagz <97088312+Air-bagz@users.noreply.github.com>
2022-03-01 22:32:35 +0800

    Update 7.5.2 SelectSort2.cpp

--- 7.5.2 SelectSort2.cpp
+++ 7.5.2 SelectSort2.cpp
@@ -17,7 +17,7 @@ void SelectSort(int A[],int n)
 	for (int i=1; i<=n-1; i++)	//一共进行n-1趟 
 	{
 		int min=i;		//记录最小元素位置
-		for (int j=i+1; j<=n; j++)	//在A[i...n-1]中选择更小的元素
+		for (int j=i+1; j<=n; j++)	//在A[i+1...n]中选择更小的元素
 			if (A[j]<A[min])	min=j;	//更新最小元素位置
 		if (min!=i)	swap(A[i],A[min]);	//封装的swap()函数共移动元素3次 
 	}
